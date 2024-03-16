22bf3baf16851b53a8d7345d3e5461abf23e542f
Phil Tyler <laidbackcoder@gmail.com>
2024-03-16 18:33:45 +0000

    Refactored  for PEP8 line length constraints

--- src/Jellyfin-DvrEpisodeRenamer.py
+++ src/Jellyfin-DvrEpisodeRenamer.py
@@ -4,7 +4,7 @@ This application can be used to automatically rename a Jellyfin recorded TV
 file using the metadata stored in a .inf file which is automatically saved
 alongside the video file by Jellyfin's DVR functionality.
 
-Version: 1.0.0
+Version: 1.1.0
 License: MIT License
 URL: https://github.com/laidbackcoder/Jellyfin-DvrEpisodeRenamer
 
@@ -73,7 +73,9 @@ def process_file(video_file_path):
                 if os.path.exists(show_info_file_path):
 
                     # Extract the show name from the show info file
-                    show_name = xmlET.parse(show_info_file_path).getroot().find('title').text
+                    show_name = xmlET.parse(
+                        show_info_file_path
+                        ).getroot().find('title').text
 
                     # Apply any substitutions to the show name
                     for sub in SUBSTITUTION_SHOW_NAMES:
@@ -83,76 +85,143 @@ def process_file(video_file_path):
                     print('Show Name extracted:', show_name)
 
                     # Extract the plot text from the episode info file
-                    plot = xmlET.parse(info_file_path).getroot().find('plot').text
+                    plot = xmlET.parse(
+                        info_file_path
+                        ).getroot().find('plot').text
 
-                    # Use regular expression to extract season and episode number from the plot text (adding leading zeroes if needed)
+                    # Use regular expression to extract season and episode
+                    # number from the plot text (adding leading zeroes if
+                    # needed)
                     match = re.search(r'S(\d+)\s*Ep(\d+)', plot)
                     if match:
                         season = match.group(1).zfill(2)
                         episode = match.group(2).zfill(2)
                         episode_info = f"S{season}E{episode}"
-                        print('Season and Episode info extracted:', episode_info)
+                        print('Season and Episode info extracted:',
+                              episode_info
+                              )
 
                         # Generate new name for the episode
-                        new_recording_id = '{} - {}'.format(show_name, episode_info)
+                        new_recording_id = '{} - {}'.format(
+                            show_name, episode_info
+                            )
                         if (new_recording_id != recording_id):
-                            new_video_file_name = '{}{}'.format(new_recording_id, EXTN_VIDEO_FILE)
-                            new_video_file_path = '{}/{}'.format(path, new_video_file_name)
+                            new_video_file_name = '{}{}'.format(
+                                new_recording_id, EXTN_VIDEO_FILE
+                                )
+                            new_video_file_path = '{}/{}'.format(
+                                path, new_video_file_name
+                                )
 
                             # Handle Duplicates
                             copies = 1
                             while os.path.exists(new_video_file_path):
                                 copies += 1
-                                new_video_file_name = '{}({}){}'.format(new_recording_id, copies, EXTN_VIDEO_FILE)
-                                new_video_file_path = '{}/{}'.format(path, new_video_file_name)
+                                new_video_file_name = '{}({}){}'.format(
+                                    new_recording_id, copies, EXTN_VIDEO_FILE)
+                                new_video_file_path = '{}/{}'.format(
+                                    path, new_video_file_name)
                             if (copies > 1):
-                                new_recording_id = '{}({})'.format(new_recording_id, copies)
+                                new_recording_id = '{}({})'.format(
+                                    new_recording_id, copies
+                                    )
 
                             # Rename the video file
                             try:
-                                print('Renaming Video File: from "{}" to "{}".'.format(video_file_name, new_video_file_name))
+                                print('Renaming Video File: from "{}" to "{}".'
+                                      .format(
+                                          video_file_name,
+                                          new_video_file_name
+                                          )
+                                      )
                                 os.rename(video_file_path, new_video_file_path)
                             except Exception as e:
-                                raise Exception('Unable to rename file: {}'.format(e))
+                                raise Exception(
+                                    'Unable to rename file: {}'.format(e)
+                                    )
 
-                            # Check if the episiode info file and thumbnail should be deleted or renamed
+                            # Check if the episiode info file and thumbnail
+                            # should be deleted or renamed
                             if (DELETE_INFO_AND_THUMBNAIL_FILES):
                                 # Delete the episode info file and thumbnail
                                 try:
-                                    print('Deleting Info File:', info_file_name)
+                                    print('Deleting Info File:',
+                                          info_file_name
+                                          )
                                     os.remove(info_file_path)
 
                                     if (os.path.exists(thumbnail_file_path)):
-                                        print('Deleting Thumbnail File:', thumbnail_file_name)
+                                        print('Deleting Thumbnail File:',
+                                              thumbnail_file_name
+                                              )
                                         os.remove(thumbnail_file_path)
                                 except Exception as e:
-                                    raise Exception('Unable to delete file: {}'.format(e))
+                                    raise Exception(
+                                        'Unable to delete file: {}'.format(e)
+                                        )
                             else:
                                 # Rename the episiode info file and thumbnail
                                 try:
-                                    new_info_file_name = '{}.nfo'.format(new_recording_id)
-                                    new_info_file_path = '{}/{}'.format(path, new_info_file_name)
-                                    print('Renaming Info File: from {} to {}'.format(info_file_name, new_info_file_name))
-                                    os.rename(info_file_path, new_info_file_path)
+                                    new_info_file_name = '{}.nfo'.format(
+                                        new_recording_id
+                                        )
+                                    new_info_file_path = '{}/{}'.format(
+                                        path,
+                                        new_info_file_name
+                                        )
+                                    print('Renaming Info File: from {} to {}'
+                                          .format(
+                                            info_file_name,
+                                            new_info_file_name)
+                                          )
+                                    os.rename(
+                                        info_file_path,
+                                        new_info_file_path
+                                        )
 
                                     if (os.path.exists(thumbnail_file_path)):
-                                        new_thumbnail_file_name = '{}{}'.format(new_recording_id, '-thumb.jpg')
-                                        new_thumbnail_file_path = '{}/{}'.format(path, new_thumbnail_file_name)
-                                        print('Renaming Thumbnail File: from {} to {}'.format(thumbnail_file_name, new_thumbnail_file_name))
-                                        os.rename(thumbnail_file_path, new_thumbnail_file_path)
+                                        new_thumbnail_file_name = '''
+                                        {}{}'''.format(
+                                            new_recording_id,
+                                            '-thumb.jpg'
+                                        )
+                                        new_thumbnail_file_path = '''
+                                        {}/{}'''.format(
+                                            path,
+                                            new_thumbnail_file_name
+                                            )
+                                        print(
+                                            '''Renaming Thumbnail File: from {}
+                                            to {}'''.format(
+                                                thumbnail_file_name,
+                                                new_thumbnail_file_name)
+                                            )
+                                        os.rename(
+                                            thumbnail_file_path,
+                                            new_thumbnail_file_path
+                                            )
                                 except Exception as e:
-                                    raise Exception('Unable to rename file: {}'.format(e))
+                                    raise Exception(
+                                        'Unable to rename file: {}'.format(e)
+                                        )
                             print('Done.')
                         else:
                             raise Exception('Episode already processed')
                     else:
-                        raise Exception('Season and Episode info not found in episode info file')
+                        raise Exception(
+                            '''Season and Episode info not found
+                            in episode info file'''
+                            )
                 else:
                     raise Exception('No tvshow.nfo show info file found')
             else:
                 raise Exception('No matching .nfo file found for the episode')
         else:
-            raise Exception('Invalid file format: Must be a {} file'.format(EXTN_VIDEO_FILE))
+            raise Exception(
+                'Invalid file format: Must be a {} file'.format(
+                    EXTN_VIDEO_FILE
+                    )
+                )
     except Exception as e:
         raise Exception(e)
 
@@ -198,12 +267,36 @@ errors = 0
 success = 0
 
 # Configure the Command Line Arguments
-parser = argparse.ArgumentParser(description='Auto Rename Jellyfin TV Show Recordings (DVR)')
+parser = argparse.ArgumentParser(
+    description='Auto Rename Jellyfin TV Show Recordings (DVR)'
+    )
 group = parser.add_mutually_exclusive_group()
-parser.add_argument('path', type=str, help='Path to File or Directory to Process')
-group.add_argument('-d', '--delete', action='store_true', help='Delete the episiode info file and thumbnail after processing')
-group.add_argument('-r', '--rename', action='store_true', help='Rename the episiode info file and thumbnail after processing (default)')
-parser.add_argument('-e', '--extension', type=str, choices=['.ts', '.mp4', '.m4v'], default='.ts', help='Video File Extension (default: .ts)')
+parser.add_argument(
+    'path',
+    type=str,
+    help='Path to File or Directory to Process'
+    )
+group.add_argument(
+    '-d',
+    '--delete',
+    action='store_true',
+    help='Delete the episiode info file and thumbnail after processing'
+    )
+group.add_argument(
+    '-r',
+    '--rename',
+    action='store_true',
+    help='''Rename the episiode info file and thumbnail after
+    processing (default)'''
+    )
+parser.add_argument(
+    '-e',
+    '--extension',
+    type=str,
+    choices=['.ts', '.mp4', '.m4v'],
+    default='.ts',
+    help='Video File Extension (default: .ts)'
+    )
 args = parser.parse_args()
 
 # Handle input arguments
@@ -233,8 +326,11 @@ if (os.path.exists(path)):
             print(e, '\nSkipping file...')
             errors += 1
 
-    print('\n-------------------------------------------------------------------------------')
-    print('Finished: Successfully processed {} file(s), Failed to process {} file(s)'.format(success, errors))
-    print('-------------------------------------------------------------------------------\n')
+    print('''\n-------------------------------------
+          ------------------------------------------''')
+    print('''Finished: Successfully processed {} file(s)
+          , Failed to process {} file(s)'''.format(success, errors))
+    print('''---------------------------------------
+          ----------------------------------------\n''')
 else:
     print('\nError: Could not locate file or directory:', path)
